7748a8a0e504512912865e2652d0aecdf3f3da99
Clint Modien <cmodien@gmail.com>
2019-06-05 11:25:02 -0700

    Fixes for: VR2-1234 #time 13d 1h 5m VR2-4321 #time 12d 2h 7m VR2-9876 #time 11d 3h 9m

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
-var express = require('express');
-var bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
+const issueRegex = /VR2-[0-9]+/;
+const daysRegex = /[0-9]+d/;
+const hoursRegex = /[0-9]+h/;
+const minsRegex = /[0-9]+m/;
 
 app.use(express.json())
 
+function processMessage(message) {
+  message
+}
+
 app.post('/', function(request, response){
-  console.log(request.body);
+  if (request.body.commits == null) {
+    response.end();
+    return;
+  }
+  console.log(request.body.commits)
+  for (var i = 0; i < request.body.commits.length; i++) {
+    commit = request.body.commits[i]
+    message = commit.message
+    lines = message.split('\n')
+    for (var j = 0; j < lines.length; j++) {
+      line = lines[j]
+      issue = line.match(issueRegex)
+      if (issue) {
+        console.log("issue = " + issue);
+        timeParts = line.split("#time")
+        if (timeParts.length == 2) {
+          timeLogString = timeParts[1]
+          days = timeLogString.match(daysRegex)
+          hours = timeLogString.match(hoursRegex)
+          mins = timeLogString.match(minsRegex)
+          console.log(days + ", " + hours + ", " + mins)
+        }
+      }
+    }
+  }
   response.end()
 });
 
